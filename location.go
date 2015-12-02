871571f3747da9903d38717ce4af56afdcc85d34
Jinzhu <wosmvp@gmail.com>
2015-12-02 08:27:33 +0800

    Using configure resource API

--- location.go
+++ location.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/qor/qor"
 	"github.com/qor/qor/admin"
+	"github.com/qor/qor/resource"
 	"github.com/qor/qor/utils"
 )
 
@@ -31,35 +32,37 @@ func (location *Location) GetLocation() *Location {
 
 var injected bool
 
-func (Location) ConfigureQorResource(res *admin.Resource) {
-	Admin := res.GetAdmin()
-	res.UseTheme("location")
+func (*Location) ConfigureQorResource(res resource.Resourcer) {
+	if res, ok := res.(*admin.Resource); ok {
+		Admin := res.GetAdmin()
+		res.UseTheme("location")
 
-	if !injected {
-		injected = true
-		for _, gopath := range strings.Split(os.Getenv("GOPATH"), ":") {
-			admin.RegisterViewPath(path.Join(gopath, "src/github.com/qor/location/views"))
-		}
-		Admin.RegisterFuncMap("replace_suffix", func(str, suffix, newSuffix string) string {
-			return fmt.Sprint(strings.TrimSuffix(str, suffix), newSuffix)
-		})
-	}
-
-	scope := Admin.Config.DB.NewScope(res.Value)
-	if field, ok := scope.GetModelStruct().ModelType.FieldByName("Location"); ok {
-		labelName := field.Name
-		if customName, ok := utils.ParseTagOption(field.Tag.Get("location"))["NAME"]; ok {
-			labelName = customName
+		if !injected {
+			injected = true
+			for _, gopath := range strings.Split(os.Getenv("GOPATH"), ":") {
+				admin.RegisterViewPath(path.Join(gopath, "src/github.com/qor/location/views"))
+			}
+			Admin.RegisterFuncMap("replace_suffix", func(str, suffix, newSuffix string) string {
+				return fmt.Sprint(strings.TrimSuffix(str, suffix), newSuffix)
+			})
 		}
 
-		if res.GetMeta(field.Name) == nil {
-			res.Meta(&admin.Meta{Name: field.Name, Label: labelName, Type: "location", Valuer: func(resource interface{}, ctx *qor.Context) interface{} {
-				return resource.(locationInterface).GetLocation()
-			}})
-			res.IndexAttrs(append(res.IndexAttrs(), "-"+field.Name, "-Latitude", "-Longitude")...)
-			res.ShowAttrs(append(res.ShowAttrs(), "-"+field.Name)...)
-			res.EditAttrs(append(res.EditAttrs(), "-Address", "-City", "-Region", "-Country", "-Zip", "-Latitude", "-Longitude")...)
-			res.NewAttrs(append(res.NewAttrs(), "-Address", "-City", "-Region", "-Country", "-Zip", "-Latitude", "-Longitude")...)
+		scope := Admin.Config.DB.NewScope(res.Value)
+		if field, ok := scope.GetModelStruct().ModelType.FieldByName("Location"); ok {
+			labelName := field.Name
+			if customName, ok := utils.ParseTagOption(field.Tag.Get("location"))["NAME"]; ok {
+				labelName = customName
+			}
+
+			if res.GetMeta(field.Name) == nil {
+				res.Meta(&admin.Meta{Name: field.Name, Label: labelName, Type: "location", Valuer: func(resource interface{}, ctx *qor.Context) interface{} {
+					return resource.(locationInterface).GetLocation()
+				}})
+				res.IndexAttrs(append(res.IndexAttrs(), "-"+field.Name, "-Latitude", "-Longitude")...)
+				res.ShowAttrs(append(res.ShowAttrs(), "-"+field.Name)...)
+				res.EditAttrs(append(res.EditAttrs(), "-Address", "-City", "-Region", "-Country", "-Zip", "-Latitude", "-Longitude")...)
+				res.NewAttrs(append(res.NewAttrs(), "-Address", "-City", "-Region", "-Country", "-Zip", "-Latitude", "-Longitude")...)
+			}
 		}
 	}
 }
